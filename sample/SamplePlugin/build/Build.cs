927f3cbedd428c75b2b4bd43c14ecb9d2a6d3c56
dosymep <dosymep@gmail.com>
2023-09-08 11:14:04 +0300

    add sample project

---
+++ sample/SamplePlugin/build/Build.cs
@@ -0,0 +1,84 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using dosymep.Nuke.RevitVersions;
+
+using Nuke.Common;
+using Nuke.Common.IO;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Components;
+
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
+
+class Build : NukeBuild {
+    /// Support plugins are available for:
+    /// - JetBrains ReSharper        https://nuke.build/resharper
+    /// - JetBrains Rider            https://nuke.build/rider
+    /// - Microsoft VisualStudio     https://nuke.build/visualstudio
+    /// - Microsoft VSCode           https://nuke.build/vscode
+    public static int Main() => Execute<Build>(x => x.Compile);
+
+    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    /// <summary>
+    /// Min Revit version.
+    /// </summary>
+    [Parameter] readonly RevitVersion MinVersion = RevitVersion.Rv2016;
+
+    /// <summary>
+    /// Max Revit version.
+    /// </summary>
+    [Parameter] readonly RevitVersion MaxVersion = RevitVersion.Rv2016;
+
+    /// <summary>
+    /// The name of the project to be compiled.
+    /// </summary>
+    string ProjectName => "SamplePlugin";
+
+    /// <summary>
+    /// Output directory.
+    /// </summary>
+    AbsolutePath OutputDirectory => RootDirectory / "bin";
+
+    /// <summary>
+    /// Project directory.
+    /// </summary>
+    AbsolutePath ProjectDirectory => RootDirectory / ProjectName;
+
+    /// <summary>
+    /// Cleans <see cref="Build.OutputDirectory"/> and build and obj folders in project.
+    /// </summary>
+    Target Clean => _ => _
+        .Executes(() => {
+            OutputDirectory.CreateOrCleanDirectory();
+            ProjectDirectory.GlobDirectories("**/bin", "**/obj")
+                .Where(item => item != RootDirectory / "build" / "bin")
+                .Where(item => item != RootDirectory / "build" / "obj")
+                .DeleteDirectories();
+        });
+
+    /// <summary>
+    /// Compile project with all revit versions.
+    /// </summary>
+    Target Compile => _ => _
+        .DependsOn(Clean)
+        .Executes(() => {
+            DotNetBuild(s => s
+                .EnableForce()
+                .DisableNoRestore()
+                .SetProjectFile(ProjectName)
+                .SetConfiguration(Configuration)
+                .When(IsServerBuild, _ => _
+                    .EnableContinuousIntegrationBuild())
+                // HACK: enable restore to set TargetFramework property
+                .SetProcessArgumentConfigurator(a => a.Add("--restore"))
+                .CombineWith(RevitVersion.GetRevitVersions(MinVersion, MaxVersion), (settings, version) => {
+                    return settings
+                        .SetOutputDirectory(OutputDirectory / version)
+                        .SetProperty("RevitVersion", (int) version)
+                        .SetProperty("TargetFramework", version.TargetFramework);
+                }));
+        });
+}
